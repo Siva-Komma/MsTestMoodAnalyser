577558a4ef440c3b025cd2e682e5bf258131152f
Siva-Komma <kommasivajyothi22@gmail.com>
2023-03-09 16:25:52 +0530

    Ability to Analyse Mood Happy or Sad

--- MoodAnalyzerProblem/ModdAnalyzer.cs
+++ MoodAnalyzerProblem/ModdAnalyzer.cs
@@ -9,16 +9,12 @@ namespace MoodAnalyzerProblem
 {
     public class ModdAnalyzer
     {
-<<<<<<< HEAD
-        public string AnalyseMood(string message)
-=======
         public  string message;
         public ModdAnalyzer(string message)
         {
             this.message = message;
         }
         public string AnalyseMood()
->>>>>>> UC2_RefactorMoodAnalyser
         {
             if (message.Contains("Sad"))
             {
